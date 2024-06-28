86f939645173bc01907b4eebebdd51954aec4e1a
ehdwo13 <ehdwo13@gmail.com>
2024-06-29 00:28:13 +0900

    20240629

---
+++ thisisjava/src/ch01/sec11/Hello.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+/*
+ * 	@author ehdwo13
+ * 	1장 자바 시작하기
+ * 	2024.06.27
+ * */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, java");
+	}
+}
---
+++ thisisjava/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,16 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+	
+	public static void main(String args[]) {
+		
+		int value;
+		
+		//초기화 에러
+//		int result = value + 10;
+		
+//		System.out.println(result);
+		
+	}
+
+}
